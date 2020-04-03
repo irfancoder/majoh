a04f90f22456ab52e76f67ab61299c4d2d8f1258
IrfanIsmail <irfan.ismail@github.com>
2020-04-03 13:06:43 +0800

    irfan_3/04

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Header from "./Header";
+import Footer from "./Footer";
 
-const Layout = () => {
-    return (  );
-}
- 
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <CssBaseline />
+      <Header></Header>
+      <Container maxWidth={"md"}>{children}</Container>
+      <Footer></Footer>
+    </div>
+  );
+};
+
 export default Layout;
